85746808e721b3407506a87d096548099e586974
Soudamini <soudamini@scaledge.io>
2023-02-16 12:01:32 +0530

    Pushing div file

--- design/div.sv
+++ design/div.sv
@@ -1,52 +1,42 @@
-module full_subtractor #(parameter N=16)
-  (
-    input [N-1:0] A,
-    input [N-1:0] B,
-    output [N-1:0] Diff,
-    output Borrow
-  );
-  
-  wire [N-1:0] temp;
-  
-  assign temp = A - B;
-  assign Diff = temp;
-  assign Borrow = (A < B); //result of a comparison of A and B using the less than operator A < B
-  
+module comparator(
+  input [31:0] a,
+  input [31:0] b,
+  output reg eq,
+  output reg lt
+);
+
+always @(a or b) begin
+  eq = (a == b);
+  lt = (a < b);
+end
+
 endmodule
 
-module divider#(parameter N=16)( 
-  input [N-1:0] Dividend,
-  input [N-1:0] Divisor,
-  output reg [N-1:0] Quotient,
-  output reg [N-1:0] Remainder
+/*module divider(
+  input clk,
+  input rst,
+  input en,
+  input [31:0] divide_by,
+  output reg div_out
 );
-  
-  reg [N-1:0] temp_remainder;
-  reg [N-1:0] current_dividend;
-  wire Quotient_bit;
-  
-  initial begin
-    temp_remainder = Dividend;
-    Quotient = 0;
-  end
-  
-  genvar i;
-  
-  generate
-    for(i=N-1; i>=0; i=i-1)begin
-      current_dividend = {temp_remainder[N-1:i], Quotient[i-1:0]};
-   //   current_dividend = {temp_remainder[N-1:i], Quotient[i-1:0]};
-      full_subtractor#(N) fs(
-        .A(current_dividend),
-        .B(Divisor),
-        .Diff(temp_remainder),
-        .Borrow(Quotient_bit)
-      );
-      assign Quotient[i] = Quotient_bit;
-    end
-  endgenerate
 
-  assign Remainder = temp_remainder;
-  
+reg [31:0] counter = 0;
+comparator comp (
+  .a(counter),
+  .b(divide_by),
+  .eq(div_out),
+  .lt(counter_next)
+);
+
+always @(posedge clk) begin
+  if (rst) begin
+    counter <= 0;
+  end else if (en) begin
+    if (counter_next) begin
+      counter <= counter + 1;
+    end
+  end
+end
+
 endmodule
 
